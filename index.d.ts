4b847b80fb20da06295bf27a279a061bf9c04099
Richard X <richardx366@gmail.com>
2022-07-08 20:25:09 -0400

    fix

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,7 @@
 import { PrismaClient } from '@prisma/client';
 import NodeCache from 'node-cache';
 
-export function prismaCache(prisma: PrismaClient, cache: NodeCache): void;
+export default function prismaCache(
+  prisma: PrismaClient,
+  cache: NodeCache,
+): void;
